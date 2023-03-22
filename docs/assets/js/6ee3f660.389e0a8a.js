72c9c931eb3a74f7eaa2d91f62fa39010def7731
Borja Lanza López <bjlanza+git@gmail.com>
2023-03-22 09:54:18 +0100

    Subo versión 22
    Mejoras

---
+++ docs/assets/js/6ee3f660.389e0a8a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcyberdocs=self.webpackChunkcyberdocs||[]).push([[8744],{179:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/documentacion","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ docs/assets/js/814f3328.0d51aab8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcyberdocs=self.webpackChunkcyberdocs||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Primera Lecci\xf3n","permalink":"/blog/primera-leccion"},{"title":"Bienvenida","permalink":"/blog/bienvenida"}]}')}}]);
